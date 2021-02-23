478a160b199c4e7062e15b6cbf23c0dbc3490923
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-02-23 17:30:32 +0900

    remove new Date

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = require("./registere/bot");
 const kyoshinMonita = require("./registere/kyoshin-monita");
 
 bot.on("ready", () => {
-	kyoshinMonita.start(new Date());
+	kyoshinMonita.start();
 });
 
 bot.login(process.env.DISCORD_BOT_TOKEN);
