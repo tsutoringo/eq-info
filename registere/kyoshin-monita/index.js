c0023432350609557dc628f748d02ad8efc5ff37
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-02-27 14:27:00 +0900

    move

--- registere/kyoshin-monita/index.js
+++ registere/kyoshin-monita/index.js
@@ -1,17 +1,18 @@
-const { channels } = require("../config");
-const { Client, EEW } = require("kyoshin-monita");
-const bot = require("./bot");
-const createEmbed = require("./kyoshin-monita/createEmbed")
+const { channels } = require("../../config");
+const { Client } = require("kyoshin-monita");
+const bot = require("../bot");
+const createEmbed = require("./createEmbed");
 
 const kyoshinMonita = new Client({
 	updateFrequency: 1000
 });
 
-kyoshinMonita.on("newReport", ({eew, imageURLs}) => {
+kyoshinMonita.on("newReport", ({eew}) => {
+	
 	bot.channels.fetch(channels.kyoshinMonita).then(channel => {
 		return channel.send("", {
 			embed: createEmbed(eew)
-		})
+		});
 	}).then(message => {
 		if (eew.is.final || eew.is.firstReport || !(eew.report.num % 10)) message.crosspost();
 	});
